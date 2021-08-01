87a9a9a8bcdd10ada04266f80f5076ffbaf11c41
David OH <holifeilddavid@gmail.com>
2021-08-01 15:56:05 +0300

    Home and Header Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,66 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 function Header() {
   return (
-    <header className="bg-blue-600">
+    <header className="bg-gray-700">
       <div className="container mx-auto justify-between flex">
         <nav className="flex">
           <NavLink
             to="/"
             exact
             activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-yellow-400 text-4xl cursive tracking-widest"
+            className="inflex-flex items-center py-4 px-3 mr-4 text-gray-100 hover:text-yellow-400 text-4xl cursive tracking-widest"
           >
             Olal D.H.
           </NavLink>
           <NavLink
             to="/project"
-            activeClassName="text-blue-100 bg-blue-700"
-            className="inline-flex items-center p-3 my-6 rounded text-blue-200 hover:text-yellow-400"
+            activeClassName="text-gray-100 bg-gray-600"
+            className="inline-flex items-center p-2 my-3 rounded text-gray-200 hover:text-yellow-400"
           >
             Projects
           </NavLink>
           <NavLink
             to="/blog"
-            activeClassName="text-blue-100 bg-blue-700"
-            className="inline-flex items-center p-3 my-6 rounded text-blue-200 hover:text-yellow-400"
+            activeClassName="text-gray-100 bg-gray-600"
+            className="inline-flex items-center p-2 my-3 rounded text-gray-200 hover:text-yellow-400"
           >
             Blog
           </NavLink>
           <NavLink
             to="/about"
-            activeClassName="text-blue-100 bg-blue-700"
-            className="inline-flex items-center p-3 my-6 rounded text-blue-200 hover:text-yellow-400"
+            activeClassName="text-gray-100 bg-gray-600"
+            className="inline-flex items-center p-3 my-3 rounded text-gray-200 hover:text-yellow-400"
           >
             About Me!
           </NavLink>
         </nav>
+
+        <div className="inline-flex p-2 my-3">
+          <SocialIcon
+            url="https://github.com/dolal1"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fffff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="twitter.com/HolifieldOlal"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fffff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.instagram.com/_the_ace_of_spades_/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fffff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
       </div>
     </header>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
 
+import image from "../assets/grey wood.jpg";
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <img
+        src={image}
+        alt="Background Skies"
+        className="absolute object-cover w-full h-full"
+      />
+
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-5xl text-green-200 font-bold cursive leading-none lg:leading-snug home-name">
+          Olal David Holifield
+        </h1>
+        <h4 className="text-3xl text-green-200 font-bold mt-auto mb-auto">
+          I'm a Web Developer
+        </h4>
+      </section>
+    </main>
   );
 }
 
